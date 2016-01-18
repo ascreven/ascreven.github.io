c0e27e111f6ed44108da28b27dde89a8f50f51b9
Alexandra <akscreven@gmail.com>
2016-01-18 13:36:32 -0500

    added about page

--- js/app.js
+++ js/app.js
@@ -8,20 +8,26 @@
   ])
   .config([
     '$stateProvider',
+    '$locationProvider',
     RouterFunction
   ]);
 
-  function RouterFunction($stateProvider){
+  function RouterFunction($stateProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
     $stateProvider
     .state('home',{
       url: '/',
       templateUrl: 'js/home/index.html'
     })
-    .state('projectIndex',{
+    .state('projects',{
       url: '/projects',
-      templateUrl: 'js/projects/index.html',
+      templateUrl: 'js/projects/projects.html',
       controller: 'projectsController',
       controllerAs: 'projectsViewModel'
+    })
+    .state('about',{
+      url: '/about',
+      template: "I'm in the about page"
     });
   }
 })();
